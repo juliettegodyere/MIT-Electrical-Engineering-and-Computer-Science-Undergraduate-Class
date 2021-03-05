54749beef902de99b9bb9dc03e808c2e428eeaa3
juliettegodyere <juliettejuli@yahoo.com>
2021-03-05 16:20:12 +0100

    first commit

---
+++ Year 1/Intro to Computer Science & Programming/Problem Sets/ps0/ps0.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  2 18:13:11 2021
+
+@author: nkworjuliet
+"""
+
+#
+import math
+
+x = int(input("Enter a value...."))
+y = int(input("Enter a value...."))
+pow = x**y
+print(pow)
+log = int(math.log(x, 2))
+print(log)
---
+++ Year 1/Intro to Computer Science & Programming/Problem Sets/ps0/ps1a.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  4 00:29:26 2021
+
+@author: nkworjuliet
+"""
+
